fe80f1dc7be5e3c376017b2d4bd7250d62323fea
elvysd <elvysd@gmail.com>
2021-07-01 20:58:50 -0700

    Major changes

--- button.js
+++ button.js
@@ -11,19 +11,26 @@ const btn = document.querySelector("#btn");
 btn.onclick = alertFunction;
 console.log(3);
 
-for (let i = 0; i < 16; i++) {
+for (let i = 0; i < 64; i++) {
   console.log(i);
   // select id
   const container = document.querySelector('#container');
   // add element div 
   const content = document.createElement('div');
-  // adds class "content"
-  content.classList.add('content');
-  // adds text content
-  content.textContent = '';
-  // adds actual content
-  container.appendChild(content);
+  // adds class "flex-container"
+  if (i % 2 == 0) {
+    content.classList.add('flex-container' + 1);
+    // adds text content
+    content.textContent = 'i' + i;
+    // adds actual content
+    container.appendChild(content);
+  }
+  else {
+    content.classList.add('flex-container');
+    // adds text content
+    content.textContent = 'i' + i;
+    // adds actual content
+    container.appendChild(content);
+  }
 }
-
-
 
